Corrected some errors in the code related to the 3rd order cumulant linear spectrum. Currently gbom_cumulant_response contains a Hack that speeds up the calculation of h3 by ignoring the 3-phonon term. Fix later!

generate_spectra.py
@@ -5,6 +5,7 @@ import sys
 import os
 import numpy as np
 import math
+from numba import config
 import spec_pkg.constants.constants as const
 from spec_pkg.GBOM import gbom
 from spec_pkg.GBOM import extract_model_params_gaussian as gaussian_params
@@ -83,28 +84,37 @@ def compute_GBOM_absorption(param_list,GBOM_chromophore,solvent,is_emission):
 			np.savetxt(param_list.GBOM_root+'_spectral_density_exact_corr.dat', GBOM_chromophore.spectral_dens)
 			GBOM_chromophore.calc_g2_qm(param_list.temperature,param_list.num_steps,param_list.max_t,is_emission)
 			#save lineshape function:
-			np.savetxt(param_list.GBOM_root+'_lineshape_func_2nd_order_exact_corr.dat',(GBOM_chromophore.g2_exact).real)
+			#np.savetxt(param_list.GBOM_root+'_lineshape_func_2nd_order_exact_corr.dat',(GBOM_chromophore.g2_exact).real)
 
 			# only compute third order cumulant if needed
 			if param_list.third_order:
 				GBOM_chromophore.calc_g3_qm(param_list.temperature,param_list.num_steps,param_list.max_t,is_emission)
 				# sace lineshape function
-				temp_lineshape=(GBOM_chromophore.g2_exact).real
-				temp_lineshape[:,1]=temp_lineshape[:,1]+(GBOM_chromophore.g3_exact[:,1]).real
-				np.savetxt(param_list.GBOM_root+'_lineshape_func_3rd_order_exact_corr.dat',temp_lineshape)
+				#temp_lineshape2=(GBOM_chromophore.g2_exact).real
+				#temp_lineshape2[:,1]=(GBOM_chromophore.g2_exact[:,1]).real+(GBOM_chromophore.g3_exact[:,1]).real
+				#np.savetxt(param_list.GBOM_root+'_lineshape_func_3rd_order_exact_corr.dat',temp_lineshape2)
+
+				# also print only g:
+				#temp_lineshape2=(GBOM_chromophore.g3_exact).real
+				#np.savetxt(param_list.GBOM_root+'_g3_exact_corr_real.dat',temp_lineshape2)
+				#temp_lineshape2=(GBOM_chromophore.g3_exact).real
+				#temp_lineshape2[:,1]=(GBOM_chromophore.g3_exact[:,1]).imag
+				#np.savetxt(param_list.GBOM_root+'_g3_exact_corr_imag.dat',temp_lineshape2)
+
 		else:
 			GBOM_chromophore.calc_spectral_dens(param_list.temperature,param_list.max_t,param_list.num_steps,param_list.decay_length,True,is_emission)
 			np.savetxt(param_list.GBOM_root+'_spectral_density_harmonic_qcf.dat', GBOM_chromophore.spectral_dens)
 			GBOM_chromophore.calc_g2_cl(param_list.temperature,param_list.num_steps,param_list.max_t,is_emission)
 			#save lineshape function:
-			np.savetxt(param_list.GBOM_root+'_lineshape_func_2nd_order_harmonic_qcf.dat',(GBOM_chromophore.g2_cl).real)
+			#np.savetxt(param_list.GBOM_root+'_lineshape_func_2nd_order_harmonic_qcf.dat',(GBOM_chromophore.g2_cl).real)
 
 			if param_list.third_order:
 				GBOM_chromophore.calc_g3_cl(param_list.temperature,param_list.num_steps,param_list.max_t,is_emission)
 				# sace lineshape function
-				temp_lineshape=(GBOM_chromophore.g2_cl).real
-				temp_lineshape[:,1]=temp_lineshape[:,1]+(GBOM_chromophore.g3_cl[:,1]).real
-				np.savetxt(param_list.GBOM_root+'_lineshape_func_3rd_order_harmonic_qcf.dat',temp_lineshape)
+				#temp_lineshape=(GBOM_chromophore.g2_cl).real
+				#temp_lineshape[:,1]=temp_lineshape[:,1]+(GBOM_chromophore.g3_cl[:,1]).real
+				#np.savetxt(param_list.GBOM_root+'_lineshape_func_3rd_order_harmonic_qcf.dat',temp_lineshape)
+
 
 		GBOM_chromophore.calc_cumulant_response(param_list.third_order,param_list.exact_corr,is_emission)	
 		spectrum=linear_spectrum.full_spectrum(GBOM_chromophore.cumulant_response,solvent.solvent_response,param_list.num_steps,E_start,E_end,True)
@@ -238,7 +248,7 @@ def compute_MD_absorption(param_list,MDtraj,solvent,is_emission):
 			MDtraj.calc_3rd_order_corr(param_list.corr_length_3rd)
 			# technically, in 3rd order cumulant, can have 2 different temperatures again. one at
 			# which the MD was performed and one at wich the spectrum is simulated. Fix this...
-			MDtraj.calc_g3(param_list.temperature,param_list.max_t,param_list.num_steps)
+			MDtraj.calc_g3(param_list.temperature,param_list.max_t,param_list.num_steps,param_list.low_freq_cutoff)
 			MDtraj.calc_cumulant_response(True,is_emission)
 		else:
 			MDtraj.calc_cumulant_response(False,is_emission)
@@ -265,6 +275,12 @@ def compute_MD_absorption(param_list,MDtraj,solvent,is_emission):
 
 # main driver #
 input_file=sys.argv[1]
+if len(sys.argv)<3:
+	num_cores=1
+else:
+	num_cores=int(sys.argv[2])
+
+config.NUMBA_NUM_THREADS=num_cores
 
 # parse input values
 if os.path.exists(input_file):
@@ -296,8 +312,16 @@ if param_set.model=='GBOM':
 			freqs_gs=gaussian_params.extract_normal_mode_freqs(param_set.GBOM_root+'_gs.log',param_set.num_modes,param_set.num_frozen_atoms)
 			freqs_ex=gaussian_params.extract_normal_mode_freqs(param_set.GBOM_root+'_ex.log',param_set.num_modes,param_set.num_frozen_atoms)
 			K=gaussian_params.extract_Kmat(param_set.GBOM_root+'_vibronic.log',param_set.num_modes)
-			J=gaussian_params.extract_duschinsky_mat(param_set.GBOM_root+'_vibronic.log',param_set.num_modes)
+			J=np.zeros((freqs_gs.shape[0],freqs_gs.shape[0]))
+			if param_set.no_dusch:
+				counter=0
+				while counter<freqs_ex.shape[0]:
+					J[counter,counter]=1.0
+					counter=counter+1
+			else:
+				J=gaussian_params.extract_duschinsky_mat(param_set.GBOM_root+'_vibronic.log',param_set.num_modes)
 			param_set.E_adiabatic=gaussian_params.extract_adiabatic_freq(param_set.GBOM_root+'_vibronic.log')
+
 			GBOM=gbom.gbom(freqs_gs,freqs_ex,J,K,param_set.E_adiabatic,param_set.dipole_mom)
 
 		elif param_set.Jpath!='' and param_set.Kpath!='' and param_set.freq_gs_path!='' and param_set.freq_ex_path!='':
@@ -305,9 +329,16 @@ if param_set.model=='GBOM':
 				sys.exit('Error: Did not provide dipole moment or adiabatic energy gap for GBOM!')
 			# create GBOM from input J, K and freqs
 			else:
-				J=np.genfromtxt(param_set.Jpath)
-				K=np.genfromtxt(param_set.Kpath)
 				freqs_gs=np.genfromtxt(param_set.freq_gs_path)
+				J=np.zeros((freqs_gs.shape[0],freqs_gs.shape[0]))
+                        	if param_set.no_dusch:
+                                	counter=0
+                                	while counter<freqs_gs.shape[0]:
+                                        	J[counter,counter]=1.0
+						counter=counter+1
+                        	else:
+					J=np.genfromtxt(param_set.Jpath)
+				K=np.genfromtxt(param_set.Kpath)
 				freqs_ex=np.genfromtxt(param_set.freq_ex_path)
 				# created appropriate matrices: now create GBOM.
  
@@ -501,23 +532,29 @@ elif param_set.task=='2DES':
                                         GBOM.h5_exact[:,:,2]=GBOM.h5_exact[:,:,2]+temp_imag[:,:,2]
 
 				else:
-					GBOM.calc_h1_qm(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h1_qm(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h1_real.dat',GBOM.h1_exact,False)
                                         twoDES.print_2D_spectrum('h1_imag.dat',GBOM.h1_exact,True)
-					GBOM.calc_h2_qm(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h2_qm(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h2_real.dat',GBOM.h2_exact,False)
                                         twoDES.print_2D_spectrum('h2_imag.dat',GBOM.h2_exact,True)
-					GBOM.calc_h4_qm(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h4_qm(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h4_real.dat',GBOM.h4_exact,False)
                                         twoDES.print_2D_spectrum('h4_imag.dat',GBOM.h4_exact,True)
-					GBOM.calc_h5_qm(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h5_qm(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h5_real.dat',GBOM.h5_exact,False)
                                         twoDES.print_2D_spectrum('h5_imag.dat',GBOM.h5_exact,True)
 
-	
+				# now construct 3rd order correlation function. Needed to speed up evaluation of h3
+                        	#GBOM.compute_corr_func_3rd(param_set.temperature*const.kb_in_Ha,param_set.num_steps,param_set.max_t,True)
+				#twoDES.print_2D_spectrum('corr_func_3rd_real.dat',GBOM.corr_func_3rd_qm,False)
+                             	#twoDES.print_2D_spectrum('corr_func_3rd_imag.dat',GBOM.corr_func_3rd_qm,True)
+			print('NUMBA environment variable:')
+			print(config.NUMBA_NUM_THREADS)
+
 			if param_set.method_2DES=='2DES':
 				if param_set.third_order:
-					twoDES.calc_2DES_time_series_GBOM_3rd(q_func_eff,GBOM.g3_exact,GBOM.h1_exact,GBOM.h2_exact,GBOM.h4_exact,GBOM.h5_exact,GBOM.freqs_gs,GBOM.Omega_sq,GBOM.gamma,param_set.temperature*const.kb_in_Ha,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0,False)
+					twoDES.calc_2DES_time_series_GBOM_3rd(q_func_eff,GBOM.g3_exact,GBOM.h1_exact,GBOM.h2_exact,GBOM.h4_exact,GBOM.h5_exact,GBOM.corr_func_3rd_qm,GBOM.freqs_gs,GBOM.Omega_sq,GBOM.gamma,param_set.temperature*const.kb_in_Ha,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0,False,param_set.no_dusch)
 				else:
 					twoDES.calc_2DES_time_series(q_func_eff,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0)
 			elif param_set.method_2DES=='PUMP_PROBE':
@@ -547,7 +584,7 @@ elif param_set.task=='2DES':
 				if os.path.exists('h1_real.dat') and os.path.exists('h1_imag.dat') and os.path.exists('h2_real.dat') and os.path.exists('h2_imag.dat') and os.path.exists('h4_real.dat') and os.path.exists('h4_imag.dat') and os.path.exists('h5_real.dat') and os.path.exists('h5_imag.dat'):
                                 	# read in all files:
 					GBOM.h1_cl=twoDES.read_2D_spectrum('h1_real.dat',param_set.num_steps)
-                                	GBOM.h1_cl[:,:,2]=GBOM.h2_cl[:,:,2]+1j*(twoDES.read_2D_spectrum('h1_imag.dat',param_set.num_steps))[:,:,2]
+                                	GBOM.h1_cl[:,:,2]=GBOM.h1_cl[:,:,2]+1j*(twoDES.read_2D_spectrum('h1_imag.dat',param_set.num_steps))[:,:,2]
                                 	GBOM.h2_cl=twoDES.read_2D_spectrum('h2_real.dat',param_set.num_steps)
                                 	GBOM.h2_cl[:,:,2]=GBOM.h2_cl[:,:,2]+1j*(twoDES.read_2D_spectrum('h2_imag.dat',param_set.num_steps))[:,:,2]
 
@@ -558,23 +595,26 @@ elif param_set.task=='2DES':
 
 				else:
 					# Calc h and save to file
-					GBOM.calc_h1_cl(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h1_cl(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h1_real.dat',(GBOM.h1_cl),False)
                                         twoDES.print_2D_spectrum('h1_imag.dat',GBOM.h1_cl,True)
-					GBOM.calc_h2_cl(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h2_cl(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h2_real.dat',(GBOM.h2_cl),False)
                                         twoDES.print_2D_spectrum('h2_imag.dat',GBOM.h2_cl,True)
-					GBOM.calc_h4_cl(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h4_cl(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h4_real.dat',(GBOM.h4_cl),False)
                                         twoDES.print_2D_spectrum('h4_imag.dat',GBOM.h4_cl,True)
-					GBOM.calc_h5_cl(param_set.temperature,param_set.num_steps,param_set.max_t)
+					GBOM.calc_h5_cl(param_set.temperature,param_set.num_steps,param_set.max_t,param_set.no_dusch)
 					twoDES.print_2D_spectrum('h5_real.dat',(GBOM.h5_cl),False)
-                                        twoDES.print_2D_spectrum('h5_imag.dat',GBOM.h5_cl,True)
+                                        twoDES.print_2D_spectrum('h5_imag.dat',GBOM.h5_cl,True,)
 	
-
+				# now construct 3rd order correlation function. Needed to speed up evaluation of h3
+				#GBOM.compute_corr_func_3rd(param_set.temperature*const.kb_in_Ha,param_set.num_steps,param_set.max_t,False)
+	
 			if param_set.method_2DES=='2DES':
 				if param_set.third_order:
-					twoDES.calc_2DES_time_series_GBOM_3rd(q_func_eff,GBOM.g3_cl,GBOM.h1_cl,GBOM.h2_cl,GBOM.h4_cl,GBOM.h5_cl,GBOM.freqs_gs,GBOM.Omega_sq,GBOM.gamma,param_set.temperature*const.kb_in_Ha,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0,True)
+					twoDES.calc_2DES_time_series_GBOM_3rd(q_func_eff,GBOM.g3_cl,GBOM.h1_cl,GBOM.h2_cl,GBOM.h4_cl,GBOM.h5_cl,GBOM.corr_func_3rd_cl,GBOM.freqs_gs,GBOM.Omega_sq,GBOM.gamma,param_set.temperature*const.kb_in_Ha,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0,True,param_set.no_dusch)
+
 				else:
 					twoDES.calc_2DES_time_series(q_func_eff,E_start1,E_end1,E_start2,E_end2,param_set.num_steps_2DES,filename_2DES,param_set.num_time_samples_2DES,param_set.t_step_2DES,0.0)	
 			elif param_set.method_2DES=='PUMP_PROBE':
@@ -725,15 +765,22 @@ elif param_set.task=='2DES':
 			MDtraj.calc_3rd_order_corr(param_set.corr_length_3rd)
                       	# technically, in 3rd order cumulant, can have 2 different temperatures again. one at
                         # which the MD was performed and one at wich the spectrum is simulated. Fix this...
-			MDtraj.calc_g3(param_set.temperature,param_set.max_t,param_set.num_steps)
-			MDtraj.calc_corr_func_3rd_qm_freq(param_set.temperature_MD)
+			MDtraj.calc_g3(param_set.temperature,param_set.max_t,param_set.num_steps,param_set.low_freq_cutoff)
+			MDtraj.calc_corr_func_3rd_qm_freq(param_set.temperature_MD,param_set.low_freq_cutoff)
 			# Check if h1 and h2 are already computed and stored. computational savings...
-			if os.path.exists('h1_real.dat') and os.path.exists('h1_imag.dat') and os.path.exists('h2_real.dat') and os.path.exists('h2_imag.dat'):
+			if os.path.exists('h1_real.dat') and os.path.exists('h1_imag.dat') and os.path.exists('h2_real.dat') and os.path.exists('h2_imag.dat') and os.path.exists('h4_real.dat') and os.path.exists('h4_imag.dat') and os.path.exists('h5_real.dat') and os.path.exists('h5_imag.dat'):
+
 				# read in all files:
 				MDtraj.h1=twoDES.read_2D_spectrum('h1_real.dat',param_set.num_steps)
-				MDtraj.h1[:,:,2]=MD_traj.h2[:,:,2]+1j*(twoDES.read_2D_spectrum('h1_imag.dat',param_set.num_steps))[:,:,2]	
+				MDtraj.h1[:,:,2]=MDtraj.h1[:,:,2]+1j*(twoDES.read_2D_spectrum('h1_imag.dat',param_set.num_steps))[:,:,2]	
 				MDtraj.h2=twoDES.read_2D_spectrum('h2_real.dat',param_set.num_steps)
-				MDtraj.h2[:,:,2]=MD_traj.h2[:,:,2]+1j*(twoDES.read_2D_spectrum('h2_imag.dat',param_set.num_steps))[:,:,2]	
+				MDtraj.h2[:,:,2]=MDtraj.h2[:,:,2]+1j*(twoDES.read_2D_spectrum('h2_imag.dat',param_set.num_steps))[:,:,2]	
+
+				MDtraj.h4=twoDES.read_2D_spectrum('h4_real.dat',param_set.num_steps)
+                                MDtraj.h4[:,:,2]=MDtraj.h4[:,:,2]+1j*(twoDES.read_2D_spectrum('h4_imag.dat',param_set.num_steps))[:,:,2]
+				MDtraj.h5=twoDES.read_2D_spectrum('h5_real.dat',param_set.num_steps)
+                                MDtraj.h5[:,:,2]=MDtraj.h5[:,:,2]+1j*(twoDES.read_2D_spectrum('h5_imag.dat',param_set.num_steps))[:,:,2]
+
 			else:
 				MDtraj.calc_h1(param_set.max_t,param_set.num_steps)
 				twoDES.print_2D_spectrum('h1_real.dat',(MDtraj.h1),False)

spec_pkg/GBOM/gbom.py
@@ -138,6 +138,13 @@ class gbom:
 		self.corr_func_3rd_qm=np.zeros((1,1),dtype=complex)
 		self.corr_func_3rd_qm_freq=np.zeros((1,1),dtype=complex)
 
+	def compute_corr_func_3rd(self,kbT,num_points,max_t,is_qm):
+		if is_qm:
+			self.corr_func_3rd_qm=gbom_cumulant_response.full_third_order_corr_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_t,num_points,False)
+		else:
+			self_corr_func_3rd_cl=gbom_cumulant_response.full_third_order_corr_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_t,num_points,True)
+
+
 	def set_absorption_variables(self):
                 # temporary variable. Not needed to be accessible to the outside
                 omega_e_sq=get_omega_sq(self.freqs_ex)
@@ -192,40 +199,40 @@ class gbom:
 		else:
                 	self.g3_exact=gbom_cumulant_response.full_third_order_lineshape(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False)
 
-        def calc_h4_qm(self,temp,num_points,max_time):
+        def calc_h4_qm(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h4_exact=gbom_cumulant_response.full_h4_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False)
+                self.h4_exact=gbom_cumulant_response.full_h4_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False,no_dusch)
 
-        def calc_h5_qm(self,temp,num_points,max_time):
+        def calc_h5_qm(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h5_exact=gbom_cumulant_response.full_h5_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False)
+                self.h5_exact=gbom_cumulant_response.full_h5_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False,no_dusch)
 
-        def calc_h4_cl(self,temp,num_points,max_time):
+        def calc_h4_cl(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h4_cl=gbom_cumulant_response.full_h4_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True)
+                self.h4_cl=gbom_cumulant_response.full_h4_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True,no_dusch)
 
-        def calc_h5_cl(self,temp,num_points,max_time):
+        def calc_h5_cl(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h5_cl=gbom_cumulant_response.full_h5_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True)
+                self.h5_cl=gbom_cumulant_response.full_h5_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True,no_dusch)
 
 
 
 
-        def calc_h1_qm(self,temp,num_points,max_time):
+        def calc_h1_qm(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h1_exact=gbom_cumulant_response.full_h1_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False)
+                self.h1_exact=gbom_cumulant_response.full_h1_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False,no_dusch)
 
-	def calc_h2_qm(self,temp,num_points,max_time):
+	def calc_h2_qm(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h2_exact=gbom_cumulant_response.full_h2_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False)
+                self.h2_exact=gbom_cumulant_response.full_h2_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,False,no_dusch)
 
-        def calc_h1_cl(self,temp,num_points,max_time):
+        def calc_h1_cl(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h1_cl=gbom_cumulant_response.full_h1_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True)
+                self.h1_cl=gbom_cumulant_response.full_h1_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True,no_dusch)
 
-        def calc_h2_cl(self,temp,num_points,max_time):
+        def calc_h2_cl(self,temp,num_points,max_time,no_dusch):
                 kbT=const.kb_in_Ha*temp
-                self.h2_cl=gbom_cumulant_response.full_h2_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True)
+                self.h2_cl=gbom_cumulant_response.full_h2_func(self.freqs_gs,self.Omega_sq,self.gamma,kbT,max_time,num_points,True,no_dusch)
 
 	def calc_g2_qm(self,temp,num_points,max_time,is_emission):
 		kbT=const.kb_in_Ha*temp

spec_pkg/cumulant/cumulant.py
@@ -7,10 +7,10 @@ import cmath
 from scipy import integrate
 from ..constants import constants as const
 from ..GBOM import gbom_cumulant_response as gbom_cumul
-from numba import jit
+from numba import jit, njit, prange
 
-# 2D version of the simpson integral
-@jit
+# 2D version of the simpson integral. Parallelized accross cores
+@njit(fastmath=True, parallel=True)
 def simpson_integral_2D(integrant):
     limit1=integrant.shape[0]
     limit2=integrant.shape[1]
@@ -25,9 +25,7 @@ def simpson_integral_2D(integrant):
     step_x=integrant[1,0,0]-integrant[0,0,0]
     step_y=integrant[0,1,1]-integrant[0,0,1]
 
-    icount=0
-    jcount=0
-    while icount<limit1:
+    for icount in prange(limit1):
         if icount==0 or icount==limit1-1:
                 prefac_x=1.0
         elif icount%2<0.5:
@@ -35,8 +33,7 @@ def simpson_integral_2D(integrant):
         else:
                 prefac_x=4.0
 
-        jcount=0
-        while jcount<limit2:
+        for jcount in range(limit2):
 
                 if jcount==0 or jcount==limit1-1:
                         prefac_y=1.0
@@ -45,16 +42,13 @@ def simpson_integral_2D(integrant):
                 else:
                         prefac_y=4.0
 
-                result=result+prefac_x*prefac_y*integrant[icount,jcount,2]
+                result+=prefac_x*prefac_y*integrant[icount,jcount,2]
 
-
-                jcount=jcount+1
-        icount=icount+1
     return step_x*step_y*result/(9.0)
 
 # construct the effective quantum correlation function in frequency space using the Jung prefactor.
 # This is necessary for 2DES calculations.
-def construct_corr_func_3rd_qm_freq(corr_func,kbT,sampling_rate_in_fs):
+def construct_corr_func_3rd_qm_freq(corr_func,kbT,sampling_rate_in_fs,low_freq_filter):
     step_length_corr=corr_func[1,1,0]-corr_func[0,0,0]
     sample_rate=sampling_rate_in_fs*math.pi*2.0*const.hbar_in_eVfs
 
@@ -94,13 +88,16 @@ def construct_corr_func_3rd_qm_freq(corr_func,kbT,sampling_rate_in_fs):
 		eff_corr[icount,jcount,1]=freq_list[jcount]
 		prefac=prefactor_jung(freq_list[icount],freq_list[jcount],kbT)
                 eff_corr[icount,jcount,2]=(prefac*corr_func_freq[icount,jcount]).real
+		# now apply low freq filter:
+                if abs(freq_list[icount])<low_freq_filter and abs(freq_list[jcount])<low_freq_filter:
+                        eff_corr[icount,jcount,2]=0.0
                 jcount=jcount+1
         icount=icount+1
 
     return eff_corr
 
 # function constructing the complete 3rd order lineshape function from a correlation function
-def compute_lineshape_func_3rd(corr_func,kbT,sampling_rate_in_fs,max_t,steps):
+def compute_lineshape_func_3rd(corr_func,kbT,sampling_rate_in_fs,max_t,steps,low_freq_filter):
     g_func=np.zeros((steps,2),dtype=complex)
     # need to constuct 3rd order correlation function in the frequency domain. 
     step_length_corr=corr_func[1,1,0]-corr_func[0,0,0]
@@ -146,6 +143,10 @@ def compute_lineshape_func_3rd(corr_func,kbT,sampling_rate_in_fs,max_t,steps):
         jcount=0
         while jcount<corr_func_freq.shape[0]:
                 corr_func_freq[icount,jcount]=corr_func_freq[icount,jcount].real
+		# now apply low freq filter:
+		if abs(freq_list[icount])<low_freq_filter and abs(freq_list[jcount])<low_freq_filter:
+			corr_func_freq[icount,jcount]=0.0
+		
                 jcount=jcount+1
         icount=icount+1
 
@@ -251,7 +252,7 @@ def compute_h3_val(qm_corr_func_freq,t1,t2,t3):
 
 # the analogous routine to the second order lineshape integrant. We have to carefully treat the limits of the integrant
 # when omega1 or omega2=0, or when omega1=-omega2. Double checked all limits. They seem to be correct.   
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_3rd_order_cumulant_lineshape(corr_func_freq,freq_list,t_val,kbT):
     tol=10e-15
     integrant=np.zeros((corr_func_freq.shape[0],corr_func_freq.shape[0],3),dtype=complex)
@@ -292,7 +293,7 @@ def integrant_3rd_order_cumulant_lineshape(corr_func_freq,freq_list,t_val,kbT):
 # function evaluates the integrant for the auxilliary term h1 needed when constructing the 3rd order cumulant correction
 # to the 3rd order response function. Note that this function takes as an input the approximately reconstructed QUANTUM
 # correlation function ins frequency space. 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_h1(qm_corr_func_freq,t1,t2):
     tol=10e-15
     integrant=np.zeros((qm_corr_func_freq.shape[0],qm_corr_func_freq.shape[0],3),dtype=complex)
@@ -316,7 +317,7 @@ def integrant_h1(qm_corr_func_freq,t1,t2):
 # function evaluates the integrant for the auxilliary term h2 needed when constructing the 3rd order cumulant correction
 # to the 3rd order response function. Note that this function takes as an input the approximately reconstructed QUANTUM
 # correlation function ins frequency space. 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_h2(qm_corr_func_freq,t1,t2):
     tol=10e-15
     integrant=np.zeros((qm_corr_func_freq.shape[0],qm_corr_func_freq.shape[0],3),dtype=complex)
@@ -338,7 +339,7 @@ def integrant_h2(qm_corr_func_freq,t1,t2):
     return integrant
 
 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_h4(qm_corr_func_freq,t1,t2):
     tol=10e-15
     integrant=np.zeros((qm_corr_func_freq.shape[0],qm_corr_func_freq.shape[0],3),dtype=complex)
@@ -360,7 +361,7 @@ def integrant_h4(qm_corr_func_freq,t1,t2):
     return integrant
 
 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_h5(qm_corr_func_freq,t1,t2):
     tol=10e-15
     integrant=np.zeros((qm_corr_func_freq.shape[0],qm_corr_func_freq.shape[0],3),dtype=complex)
@@ -385,7 +386,7 @@ def integrant_h5(qm_corr_func_freq,t1,t2):
 # function evaluates the integrant for the auxilliary term h3 needed when constructing the 3rd order cumulant correction
 # to the 3rd order response function. Note that this function takes as an input the approximately reconstructed QUANTUM
 # correlation function ins frequency space. 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_h3(qm_corr_func_freq,t1,t2,t3):
     tol=10e-15
     integrant=np.zeros((qm_corr_func_freq.shape[0],qm_corr_func_freq.shape[0],3),dtype=complex)
@@ -407,7 +408,7 @@ def integrant_h3(qm_corr_func_freq,t1,t2,t3):
     return integrant
 
 # prefactor defined by Jung that transforms the classical correlation function in Frequency space to its QM counterpart
-@jit
+@njit(fastmath=True, parallel=True)
 def prefactor_jung(omega1,omega2,kbT):
     omega_bar=omega1+omega2
     tol=10e-15
@@ -574,7 +575,7 @@ def compute_2nd_order_cumulant_from_spectral_dens(spectral_dens,kbT,max_t,steps)
         counter=counter+1
     return q_func
 
-@jit
+@jit(fastmath=True, parallel=True)
 def integrant_2nd_order_cumulant_lineshape(spectral_dens,t_val,kbT):
     integrant=np.zeros((spectral_dens.shape[0],2),dtype=complex)
     counter=0

spec_pkg/cumulant/md_traj.py
@@ -128,10 +128,10 @@ class MDtrajs:
 		self.g3=np.zeros((1,1))
 
 		# 2DES 3rd order cumulant lineshape functions
-		self.h1=np.zeros((1,1,1))
-		self.h2=np.zeros((1,1,1))
-		self.h4=np.zeros((1,1,1))
-		self.h5=np.zeros((1,1,1))
+		self.h1=np.zeros((1,1,1),dtype=complex)
+		self.h2=np.zeros((1,1,1),dtype=complex)
+		self.h4=np.zeros((1,1,1),dtype=complex)
+		self.h5=np.zeros((1,1,1),dtype=complex)
 	
 		# response functions
 		self.ensemble_response=np.zeros((1,1))
@@ -152,16 +152,15 @@ class MDtrajs:
 		kbT=temp*const.kb_in_Ha
 		self.g2=cumulant.compute_2nd_order_cumulant_from_spectral_dens(self.spectral_dens,kbT,max_t,num_steps)
 
-	def calc_corr_func_3rd_qm_freq(self,temp):
+	def calc_corr_func_3rd_qm_freq(self,temp,low_freq_filter):
 		kbT=temp*const.kb_in_Ha
 		sampling_rate_in_fs=1.0/(self.time_step*const.fs_to_Ha)
-		self.corr_func_3rd_qm_freq=cumulant.construct_corr_func_3rd_qm_freq(self.corr_func_3rd_cl,kbT,sampling_rate_in_fs)
+		self.corr_func_3rd_qm_freq=cumulant.construct_corr_func_3rd_qm_freq(self.corr_func_3rd_cl,kbT,sampling_rate_in_fs,low_freq_filter)
 
-
-	def calc_g3(self,temp,max_t,num_steps):
+	def calc_g3(self,temp,max_t,num_steps,low_freq_filter):
 		kbT=temp*const.kb_in_Ha
 		sampling_rate_in_fs=1.0/(self.time_step*const.fs_to_Ha)
-		self.g3=cumulant.compute_lineshape_func_3rd(self.corr_func_3rd_cl,kbT,sampling_rate_in_fs,max_t,num_steps)
+		self.g3=cumulant.compute_lineshape_func_3rd(self.corr_func_3rd_cl,kbT,sampling_rate_in_fs,max_t,num_steps,low_freq_filter)
 
 	def calc_h1(self,max_t,num_steps):
 		self.h1=cumulant.compute_h1_func(self.corr_func_3rd_qm_freq,max_t,num_steps)

spec_pkg/nonlinear_spectrum/twoDES.py
@@ -6,7 +6,7 @@ import math
 import cmath
 from scipy import integrate
 import time
-from numba import jit
+from numba import config,jit, njit
 from ..cumulant import cumulant as cumul
 from ..GBOM import gbom_cumulant_response as cumul_gbom
 
@@ -34,7 +34,7 @@ def print_2D_spectrum(filename,spectrum_2D,is_imag):
 def read_2D_spectrum(filename,num_points):
         line_data=open(filename,"r")
         lines=line_data.readlines()
-        outarray=np.zeros((num_points,num_points,3))
+        outarray=np.zeros((num_points,num_points,3),dtype=complex)
         icount=0
         jcount=0
         while icount<num_points:
@@ -226,7 +226,7 @@ def calc_2DES_time_series_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,corr
 # basic calculation routines:
 # calculate a full series of 2DES spectra, sampled with a certain delay time step
 # this routine is specifically for the 3rd order cumulant correction and the GBOM
-def calc_2DES_time_series_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,Omega_sq,gamma,kbT,E_min1,E_max1,E_min2,E_max2,num_points_2D,rootname,num_times,time_step,mean,is_cl):
+def calc_2DES_time_series_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,corr_func,freqs_gs,Omega_sq,gamma,kbT,E_min1,E_max1,E_min2,E_max2,num_points_2D,rootname,num_times,time_step,mean,is_cl,no_dusch):
         averaged_val=np.zeros((num_times,2))
         transient_abs=np.zeros((num_times,num_points_2D,3))
 
@@ -240,7 +240,7 @@ def calc_2DES_time_series_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func
         counter=0
         while counter<num_times:
                 print(counter,current_delay)
-                spectrum_2D=calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,Omega_sq,gamma,kbT,current_delay,current_delay_index,E_min1,E_max1,E_min2,E_max2,num_points_2D,mean,is_cl)
+                spectrum_2D=calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,corr_func,freqs_gs,Omega_sq,gamma,kbT,current_delay,current_delay_index,E_min1,E_max1,E_min2,E_max2,num_points_2D,mean,is_cl,no_dusch)
                 print_2D_spectrum(rootname+'_2DES_'+str(counter)+'.dat',spectrum_2D,False)
 
                 #compute transient absorption contribution here:
@@ -420,7 +420,7 @@ def calc_2D_spectrum_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,corr_func
 
 # Calculation routine for the full spectrum in the 3rd order cumulant approximation for a GBOM
 @jit
-def calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,Omega_sq,gamma,kbT,delay_time,delay_index,E_min1,E_max1,E_min2,E_max2,num_points_2D,mean,is_cl):
+def calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,corr_func,freqs_gs,Omega_sq,gamma,kbT,delay_time,delay_index,E_min1,E_max1,E_min2,E_max2,num_points_2D,mean,is_cl,no_dusch):
     full_2D_spectrum=np.zeros((num_points_2D,num_points_2D,3))
     counter1=0
     counter2=0
@@ -428,7 +428,8 @@ def calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,freq
     print 'Compute rfunc'
     rfunc=calc_Rfuncs_tdelay(q_func,delay_time,delay_index)
     print 'Compute rfunc 3rd'
-    rfunc_3rd=calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,Omega_sq,gamma,kbT,delay_time,delay_index,is_cl)
+    rfunc_3rd=calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,corr_func,freqs_gs,Omega_sq,gamma,kbT,delay_time,delay_index,is_cl,no_dusch)
+
     print 'DONE'
     print 'Dimensions RFunc and Rfunc 3rd'
     print rfunc.shape[0], rfunc_3rd.shape[0]
@@ -452,7 +453,7 @@ def calc_2D_spectrum_GBOM_3rd(q_func,g_func,h1_func,h2_func,h4_func,h5_func,freq
 
 # this function computes the R1,R2,R3 and R4 functions for a given delay time tdelay.
 # function works in the 2nd order cumulant approximation
-@jit
+@njit(fastmath=True, parallel=True)
 def calc_Rfuncs_tdelay(q_func,t_delay,steps_in_t_delay):
     # first find the effective step_length
     step_length=q_func[1,0].real-q_func[0,0].real
@@ -491,8 +492,8 @@ def calc_Rfuncs_tdelay(q_func,t_delay,steps_in_t_delay):
     return rfuncs
 
 #STUPID TEST: H4 H5 files
-@jit
-def calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,Omega_sq,gamma,kbT,t_delay,steps_in_t_delay,is_cl):
+@njit(fastmath=True, parallel=True)
+def calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,corr_func,freqs_gs,Omega_sq,gamma,kbT,t_delay,steps_in_t_delay,is_cl,no_dusch):
     # first find the effective step_length
     print 'Entering Rfuncs_3rd'
     step_length=g_func[1,0].real-g_func[0,0].real
@@ -523,6 +524,12 @@ def calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,
 
     while count1<max_index:
         count2=0
+	# initialize h3 values
+    	h31=0.0+0.0j
+     	h32=0.0+0.0j
+     	h33=0.0+0.0j
+    	h34=0.0+0.0j
+
         while count2<max_index:
                 # set all required indices and parameters.
                 rfuncs[count1,count2,0]=g_func[count1,0]
@@ -530,24 +537,51 @@ def calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,
                 t1=g_func[count1,0]
                 t12=t1+t_delay
                 t123=t12+g_func[count2,0]
-		# INCONSISTENCY BETWEEN t12 and t12_index potentially?
                 t1_index=count1
                 t12_index=count1+steps_in_t_delay
                 t123_index=t12_index+count2
-		#print 'INDICES:'
-		#print t12,(count1+steps_in_t_delay)*step_length
-                # now compute required h3 functions
-                if is_cl:
-                        h31=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t1,t12,t123)
-                        h32=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t12,t123,t1)
-                        h33=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t1,t123,t12)
-                        h34=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t123,t12,t1)
-                else:
-                        h31=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t12,t123)
-                        h32=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t12,t123,t1)
-                        h33=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t123,t12)
-                        h34=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t123,t12,t1)
 
+		# if this is a no Duschinsky rotation it is fast. No action necessary
+		if no_dusch:
+			if is_cl:
+                                h31=cumul_gbom.h3_func_cl_t_no_dusch(freqs_gs,Omega_sq,gamma,kbT,t1,t12,t123)
+                                h32=cumul_gbom.h3_func_cl_t_no_dusch(freqs_gs,Omega_sq,gamma,kbT,t12,t123,t1)
+                                h33=cumul_gbom.h3_func_cl_t_no_dusch(freqs_gs,Omega_sq,gamma,kbT,t1,t123,t12)
+                                h34=cumul_gbom.h3_func_cl_t_no_dusch(freqs_gs,Omega_sq,gamma,kbT,t123,t12,t1)
+                        else:
+                                h31=cumul_gbom.h3_func_qm_t_no_dusch(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t12,t123)
+                                h32=cumul_gbom.h3_func_qm_t_no_dusch(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t12,t123,t1)
+                                h33=cumul_gbom.h3_func_qm_t_no_dusch(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t123,t12)
+                                h34=cumul_gbom.h3_func_qm_t_no_dusch(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t123,t12,t1)
+
+
+		else:
+                	if is_cl:
+                        	h31=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t1,t12,t123)
+                        	h32=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t12,t123,t1)
+                        	h33=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t1,t123,t12)
+                        	h34=cumul_gbom.h3_func_cl_t(freqs_gs,Omega_sq,gamma,kbT,t123,t12,t1)
+                	else:
+                        	h31=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t12,t123)
+                        	h32=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t12,t123,t1)
+                        	h33=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t123,t12)
+                        	h34=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t123,t12,t1)
+
+			#h31=cumul_gbom.calc_h3_time_domain(corr_func,t1_index,t12_index,t123_index)
+			#h32=cumul_gbom.calc_h3_time_domain(corr_func,t12_index,t123_index,t1_index)
+			#h33=cumul_gbom.calc_h3_time_domain(corr_func,t1_index,t123_index,t12_index)
+			#h34=cumul_gbom.calc_h3_time_domain(corr_func,t123_index,t12_index,t1_index)
+
+			# sanity check.
+			#if count2<5 and count1<5: 
+			#	h31_ref=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t12,t123)
+			#	h32_ref=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t12,t123,t1)
+			#	h33_ref=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t1,t123,t12)
+			#	h34_ref=cumul_gbom.h3_func_qm_t(freqs_gs,Omega_sq,n_i_vec,gamma,kbT,t123,t12,t1)
+			#	print('h31',t1_index,t12_index,t123_index,h31,h31_ref,h31-h31_ref)
+			#	print('h32',h32,h32_ref,h32-h32_ref)
+			#	print('h33',h33,h33_ref,h33-h33_ref)
+			#	print('h34',h34,h34_ref,h34-h34_ref)
 
 # FLIP SIGN OF g_func conjugate term! This is due to of our redefinition of g
 		rfuncs[count1,count2,2]=-g_func[t1_index,1]-g_func[t12_index,1].conjugate()-g_func[t123_index,1]+h31+h1_func[t1_index,t12_index,2]-h1_func[t1_index,t123_index,2]-h2_func[t12_index,t123_index,2]-h4_func[t1_index,t123_index,2]+h4_func[t12_index,t123_index,2]-h5_func[t1_index,t12_index,2]
@@ -632,7 +666,7 @@ def calc_Rfuncs_3rd_GBOM_tdelay(g_func,h1_func,h2_func,h4_func,h5_func,freqs_gs,
 
 # this function computes the 3rd order cumulant correction for R1, R2, R3 and R4 functions for a given
 # delay time t_delay
-@jit
+@jit(fastmath=True, parallel=True)
 def calc_Rfuncs_3rd_tdelay(g_func,h1_func,h2_func,h4_func,h5_func, qm_corr_func_freq,t_delay,steps_in_t_delay):
     print('Entering Rfuncs_3rd')
     step_length=g_func[1,0].real-g_func[0,0].real
@@ -668,6 +702,7 @@ def calc_Rfuncs_3rd_tdelay(g_func,h1_func,h2_func,h4_func,h5_func, qm_corr_func_
 		t123_index=t12_index+count2
 
                 # now compute required h3 functions
+                # in this inner loop, only t123 gets increased by delta_t
  		h31=cumul.compute_h3_val(qm_corr_func_freq,t1,t12,t123)
 		h32=cumul.compute_h3_val(qm_corr_func_freq,t12,t123,t1)
 		h33=cumul.compute_h3_val(qm_corr_func_freq,t1,t123,t12)
@@ -692,7 +727,7 @@ def calc_Rfuncs_3rd_tdelay(g_func,h1_func,h2_func,h4_func,h5_func, qm_corr_func_
 
 # integrant of the 3rd order response function in the 2nd order cumulant approximation. Need to include possibility to add pulse shape
 # omega_av is already incorporated in the function q. No need to account for the mean in the equation below
-@jit
+@jit(fastmath=True, parallel=True)
 def twoD_spectrum_integrant(rfuncs,q_func,omega1,omega2,t_delay):
     step_length=rfuncs[1,0,0]-rfuncs[0,0,0]
     steps_in_t_delay=int(t_delay/step_length)  # NOTE: delay time is rounded to match with steps in the response function
@@ -718,7 +753,7 @@ def twoD_spectrum_integrant(rfuncs,q_func,omega1,omega2,t_delay):
 
 # integrant of the 3rd order response function in the 3rd order cumulant approximation. Need to include possibility to add pulse shape
 # omega_av is already incorporated in the function q. No need to account for the mean in the equation below
-@jit
+@jit(fastmath=True, parallel=True)
 def twoD_spectrum_integrant_3rd(rfuncs,rfuncs_3rd,q_func,omega1,omega2,t_delay):
     step_length=rfuncs[1,0,0]-rfuncs[0,0,0]
     steps_in_t_delay=int(t_delay/step_length)  # NOTE: delay time is rounded to match with steps in the response function