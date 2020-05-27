5c2dc683ab0f688de702bedd5a42f035ad3d6cd9
Tim Zuehlsdorff <tzuehlsdorff@ucmerced.edu>
2020-05-27 15:56:09 -0700

    Started converting from python2 to python3

---
+++ spec_pkg/GBOM/extract_model_params_from_terachem.py
@@ -0,0 +1,228 @@
+#! /usr/bin/env python
+
+import sys  
+import os.path
+import numpy as np
+import math
+import numba
+from ..constants import constants as const
+
+def get_gs_energy(input_path_gs):
+	search_phrase='FINAL ENERGY:'
+	searchfile = open(input_path_gs,"r")
+	line_count=0
+	keyword_line=999999999
+	for line in searchfile:
+		if search_phrase in line and keyword_line==999999999:
+			keyword_line=line_count
+		line_count=line_count+1
+	# if we haven't found reference geometry, break
+	if keyword_line==999999999:
+		sys.exit('Error: Could not find final ground state energy in Terachem file:  '+input_path)
+	linefile=open(input_path_gs,"r")
+	lines=linefile.readlines()
+	energy_line=lines[keyword_line].split()
+	return float(energy_line[2])
+
+# root determines which root is the root of interest. by default this is 1
+def get_ex_energy_dipole_mom(input_path_ex,root):
+	search_phrase='Final Excited State Results:'
+	searchfile = open(input_path_ex,"r")
+	line_count=0
+	keyword_line=999999999
+	for line in searchfile:
+		if search_phrase in line and keyword_line==999999999:
+			keyword_line=line_count
+		line_count=line_count+1
+	if keyword_line==999999999:
+		sys.exit('Error: Could not find excited state energy and dipole moment in Terachem file:  '+input_path)
+	linefile=open(input_path_ex,"r")
+	lines=linefile.readlines()
+	energy_line=lines[keyword_line+3+root].split()
+	energy=float(energy_line[1])
+	ex_energy=float(energy_line[2])/const.Ha_to_eV # convert to Ha 
+	dipole_mom=np.sqrt(3.0/2.0*float(energy_line[3])/ex_energy)
+	return energy,dipole_mom
+
+def get_e_adiabatic_dipole(input_path_gs,input_path_ex,root):
+	ex_energy,dipole_mom=get_ex_energy_dipole_mom(input_path_ex,root)
+	gs_energy=get_gs_energy(input_path_gs)
+	return dipole_mom,ex_energy-gs_energy
+
+# Works
+def get_specific_grad(input_path,displacement,num_atoms):
+	search_phrase1='*** Displacement '+str(displacement)+' ***'
+	search_phrase2='dE/dX            dE/dY            dE/dZ'
+
+	grad_vec=np.zeros(num_atoms*3)
+
+	searchfile = open(input_path,"r")
+	line_count=0
+	keyword_line=999999999
+	for line in searchfile:
+		if search_phrase1 in line and keyword_line==999999999:
+			keyword_line=line_count
+		line_count=line_count+1
+	total_line_number=line_count-1
+	linefile=open(input_path,"r")
+	lines=linefile.readlines()
+	# if we haven't found specific displacement, break
+	if keyword_line==999999999:
+		sys.exit('Error: Could not find gradient for Displacement '+str(displacement))
+	line_count=keyword_line
+	keyword_line2=999999999
+	while line_count<total_line_number:
+		if search_phrase2 in lines[line_count] and keyword_line2==999999999:
+			keyword_line2=line_count
+		line_count=line_count+1
+	# if we havent found the gradient following the displacement, break
+	if keyword_line2==999999999:
+		sys.exit('Error: Could not find gradient for Displacement '+str(displacement))
+	line_start=keyword_line2+1
+	# loop over number of atoms to extract the gradient vec
+	atom_count=0
+	while atom_count<num_atoms:
+		current_line=lines[line_start+atom_count].split()
+		dx=float(current_line[0])
+		dy=float(current_line[1])
+		dz=float(current_line[2])
+		grad_vec[atom_count*3]=dx
+		grad_vec[atom_count*3+1]=dy
+		grad_vec[atom_count*3+2]=dz
+		atom_count=atom_count+1
+
+	return grad_vec	
+
+#Works
+def get_hessian_from_terachem(input_path,frozen_atom_list,num_frozen_atoms):
+	num_atoms=frozen_atom_list.shape[0] # frozen atom list is a list Natoms long, where frozen atoms
+					    # are labelled by 1 and unfrozen atoms labelled by 0
+	hessian=np.zeros((num_atoms*3,num_atoms*3))
+	displacement=0.0
+	# find displacement:
+	searchphrase='Using displacements of '
+	searchfile = open(input_path,"r")
+	line_count=0
+	keyword_line=999999999
+	for line in searchfile:
+		if searchphrase in line and keyword_line==999999999:
+			keyword_line=line_count
+		line_count=line_count+1
+	searchfile.close()	
+	if keyword_line < 9999999:
+		linefile=open(input_path,"r")
+		lines=linefile.readlines()
+		keyword=lines[keyword_line].split()
+		displacement=float(keyword[3])
+	else:
+		sys.exit('Error: Could not find displacement used in numerical Hessian calculation in Terachem file')
+
+	# Now construct the Hessian. First displacement is the (x+delta x) displacement, then the (x-delta x) displacement 
+	unfrozen_atoms=num_atoms-num_frozen_atoms
+	unfrozen_count=0
+	total_atom_count=0
+	while total_atom_count<num_atoms: 
+		# check whether this is a frozen or an unfrozen atom:
+		if frozen_atom_list[total_atom_count]==0: # unfrozen
+
+			# loop over xyz coordinates:
+			xyz_count=0
+			while xyz_count<3:
+				print('Getting gradient ', unfrozen_count, ' of ',3*unfrozen_atoms)
+				grad1=get_specific_grad(input_path,unfrozen_count*2+1,num_atoms)
+				grad2=get_specific_grad(input_path,unfrozen_count*2+2,num_atoms)
+
+				# build the effective row of the Hessian through the finite difference scheme. 
+				fd_grad=(grad1-grad2)/(2.0*displacement)
+			
+				# this seems to be the correct approach. This way, off diagonal Hessian elements are the average of the numerical and the 
+				# analytical force constants
+				hessian[total_atom_count*3+xyz_count,:]=fd_grad[:]
+		
+				unfrozen_count=unfrozen_count+1
+				xyz_count=xyz_count+1
+
+		else:  # frozen atom
+			# loop over xyz coordinates:
+			xyz_count=0
+			while xyz_count<3:
+				# set block diagonal elements corresponding to purely frozen atoms to 1
+				hessian[total_atom_count*3+xyz_count,total_atom_count*3+xyz_count]=1.0
+				xyz_count=xyz_count+1
+
+		total_atom_count=total_atom_count+1
+
+	# currently the Hessian has only one off diagonal block if there are frozen atoms. Need to fix this
+	i_count=0
+	while i_count<num_atoms:
+		j_count=i_count
+		while j_count<num_atoms:
+			if frozen_atom_list[i_count]==0 and frozen_atom_list[j_count]==1: # icount refers to unfrozen and jcount refers to frozen atom
+				# make sure the hessian element with the frozen atom as its first index is set.
+				for xyz1 in range(3):
+					for xyz2 in range(3):
+						hessian[j_count*3+xyz2,i_count*3+xyz1]=hessian[i_count*3+xyz1,j_count*3+xyz2]
+			j_count=j_count+1
+		i_count=i_count+1	
+
+
+	# symmetrize hessian h_sym=0.5*(H+H.T)
+	sym_hessian=0.5*(hessian+np.transpose(hessian))
+
+	return sym_hessian
+
+# extract both list of atomic masses and reference geometry from terachem
+# Works
+def get_masses_geom_from_terachem(input_path, num_atoms):
+	geom=np.zeros((num_atoms,3))
+	masses=np.zeros(num_atoms)
+	search_phrase='*** Reference Geometry ***'
+	searchfile = open(input_path,"r")
+
+	line_count=0
+	keyword_line=999999999
+	for line in searchfile:
+		if search_phrase in line and keyword_line==999999999:
+			keyword_line=line_count
+		line_count=line_count+1
+	linefile=open(input_path,"r")
+	lines=linefile.readlines()
+	# if we haven't found reference geometry, break
+	if keyword_line==999999999:
+		sys.exit('Error: Could not find reference geometry in terachem file '+input_path)
+
+	line_start=keyword_line+2
+	atom_count=0
+	while atom_count<num_atoms:
+		current_line=lines[line_start+atom_count].split()
+		geom[atom_count,0]=float(current_line[1])
+		geom[atom_count,1]=float(current_line[2])
+		geom[atom_count,2]=float(current_line[3])
+		elem=current_line[0] # match element with mass number then store in masses array
+		if elem=='H':
+			masses[atom_count]=const.Mass_list[0]
+		elif elem=='He':
+			masses[atom_count]=const.Mass_list[1]
+		elif elem=='Li':
+			masses[atom_count]=const.Mass_list[2]
+		elif elem=='Be':
+			masses[atom_count]=const.Mass_list[3]
+		elif elem=='B':
+			masses[atom_count]=const.Mass_list[4]
+		elif elem=='C':
+			masses[atom_count]=const.Mass_list[5]
+		elif elem=='N':
+			masses[atom_count]=const.Mass_list[6]
+		elif elem=='O':
+			masses[atom_count]=const.Mass_list[7]
+		elif elem=='F':
+			masses[atom_count]=const.Mass_list[8]
+		elif elem=='Ne':
+			masses[atom_count]=const.Mass_list[9]
+		else:
+			sys.exit('Error: Could not find atomic mass for element '+elem)
+	
+		atom_count=atom_count+1
+
+	return masses,geom
+
---
+++ spec_pkg/GBOM/hessian_to_GBOM.py
@@ -0,0 +1,305 @@
+#! /usr/bin/env python
+  
+import numpy as np
+import math
+import numba
+from ..constants import constants as const
+
+#######################################################################
+#  In this module, we take the ground and excited                     #
+#  state optimized structure of a molecule, as well as                #
+#  its Hessian at both points, and use this to construct              #
+#  the ground and excited state Frequencies, J and K that             #
+#  are needed to parameterize the given GBOM. We explicitly           #
+#  consider two cases: One where no atoms are frozen in the           #
+#  geometry optimization. Then we need to project out rotation        #
+#  and translation contributions and end up with 3N-6 normal modes.   #
+#  Alternatively, if atoms are frozen, we end up with 3N_unfrozen     #
+#  modes.                                                             #
+#######################################################################
+
+def construct_freqs_J_K(coords_gs,coords_ex,hessian_gs,hessian_ex,atomic_masses,frozen_atoms,frozen_atom_list):
+	gs_freqs,gs_nm=freqs_NM_from_hessian(hessian_gs, coords_gs,atomic_masses,frozen_atoms,frozen_atom_list)
+	ex_freqs,ex_nm=freqs_NM_from_hessian(hessian_ex, coords_ex,atomic_masses,frozen_atoms,frozen_atom_list)
+	if frozen_atoms>0:
+		J,K=construct_J_K(gs_nm,ex_nm,coords_gs,coords_ex,atomic_masses,True)
+	else:
+		J,K=construct_J_K(gs_nm,ex_nm,coords_gs,coords_ex,atomic_masses,True)
+	return gs_freqs,ex_freqs,J,K
+
+def construct_J_K(nm_gs,nm_ex,coords_gs,coords_ex,atomic_masses,is_atom_constraint):
+	# nm matrix is a matrix of Natoms*3,Natoms*3-Nconstraints
+	# full Duschinsky matrix acts in normal mode space, thus needs to have 3N-Nconstraints,3N-Nconstraints 
+	# dimensions nm^T*nm has the correct dimensions
+	# if is_atom_constraint is true, we do NOT rotate and shift ex coordinates to overlap with gs coordinates to 
+	# fulfill Eckart conditions. This is because that would lead to moving frozen atoms between the ground
+	# and excited state reference geometry, which is wrong.
+
+	if not is_atom_constraint and atomic_masses.shape[0]>2:
+		# first make sure that gs and exited state coords have the same COM
+		com_gs=np.zeros(3)
+		com_ex=np.zeros(3)
+		total_mass=np.sum(atomic_masses)
+		for i in range(atomic_masses.shape[0]):
+			com_gs[0]=com_gs[0]+atomic_masses[i]*coords_gs[i,0]
+			com_gs[1]=com_gs[1]+atomic_masses[i]*coords_gs[i,1]
+			com_gs[2]=com_gs[2]+atomic_masses[i]*coords_gs[i,2]
+			com_ex[0]=com_ex[0]+atomic_masses[i]*coords_ex[i,0]
+			com_ex[1]=com_ex[1]+atomic_masses[i]*coords_ex[i,1]
+			com_ex[2]=com_ex[2]+atomic_masses[i]*coords_ex[i,2]
+
+		com_gs=com_gs/total_mass
+		com_ex=com_ex/total_mass
+
+		for i in range(coords_gs.shape[0]):
+			coords_gs[i,:]=coords_gs[i,:]-com_gs[:]
+			coords_ex[i,:]=coords_ex[i,:]-com_ex[:]
+
+		# now both coordinates are shifted such that their COM overlaps. 
+		# build the Eckart rotation matrix: 
+		amat=np.zeros((3,3))
+		for i in range(coords_gs.shape[0]):
+			# loop over xyz coords
+			for j in range(3):
+				for k in range(3):
+					amat[j,k]=amat[j,k]+atomic_masses[i]*coords_ex[i,j]*coords_gs[i,k]
+
+		sym_a=np.dot(amat,np.transpose(amat))
+		inv_a=np.linalg.inv(amat)
+		evals,evecs=np.linalg.eig(sym_a)
+		# create dmat, the diagonal matrix of sqrts 
+		Dmat=np.zeros((3,3))
+		for i in range(3):
+			Dmat[i,i]=np.sqrt(evals[i])
+		# now build rotation matrix U
+		temp_mat=np.dot(Dmat,np.transpose(evecs))
+		temp_mat2=np.dot(evecs,temp_mat)
+		Tmat=np.dot(inv_a,temp_mat2)
+		print('Rotation matrix T for Eckart conditions:')
+		print(Tmat)
+		# successfully built Eckart rotation matrix. Now rotate both coords_ex and nm_ex. 
+		coords_ex_rotated=np.zeros((coords_ex.shape[0],coords_ex.shape[1]))	
+		nm_ex_rotated=np.zeros((nm_ex.shape[0],nm_ex.shape[1]))	
+		# first sort out coordinates. loop over atoms	
+		for i in range(coords_ex.shape[0]):
+			current_xyz=coords_ex[i,:]
+			coords_ex_rotated[i,:]=np.dot(Tmat,current_xyz)
+
+		# now sort out nm vector. loop over normal modes:
+		for i in range(nm_ex.shape[1]):
+			# loop over atoms
+			for j in range(coords_ex.shape[0]):
+				# find effective xyz normal mode compnent of normal mode vector i and atom j 
+				nm_xyz=np.zeros(3)
+				nm_xyz[0]=nm_ex[3*j,i]
+				nm_xyz[1]=nm_ex[3*j+1,i]
+				nm_xyz[2]=nm_ex[3*j+2,i]
+				rot_nm_xyz=np.dot(Tmat,nm_xyz)
+				nm_ex_rotated[3*j,i]=rot_nm_xyz[0]
+				nm_ex_rotated[3*j+1,i]=rot_nm_xyz[1]
+				nm_ex_rotated[3*j+2,i]=rot_nm_xyz[2]
+		# all done. Now make sure we overwrite old normal modes and xyz coordinates
+		nm_ex=nm_ex_rotated
+		coords_ex=coords_ex_rotated
+
+	Jmat=np.dot(np.transpose(nm_gs),nm_ex)  # This is Jmat as defined in Baiardi,Barone. 	
+
+	coord_diff=np.zeros(3*coords_ex.shape[0])
+	# create mass weighted difference vector between ground and excited state geometry
+	for i in range(coords_ex.shape[0]):
+		coord_diff[3*i]=np.sqrt(atomic_masses[i])*(coords_ex[i,0]-coords_gs[i,0])
+		coord_diff[3*i+1]=np.sqrt(atomic_masses[i])*(coords_ex[i,1]-coords_gs[i,1])
+		coord_diff[3*i+2]=np.sqrt(atomic_masses[i])*(coords_ex[i,2]-coords_gs[i,2])
+		
+	# now build Kvector.
+	Kvec=np.sqrt(1.0/const.emass_in_au)*np.dot(np.transpose(nm_gs),coord_diff) # This again follows the Barone definition. 
+	return Jmat,Kvec
+
+def moments_of_inertia(coords,atomic_masses):
+	natoms=atomic_masses.shape[0]
+	total_mass=np.sum(atomic_masses)
+	# find center of mass
+	com=np.zeros(3)
+	for i in range(atomic_masses.shape[0]):
+		com[0]=com[0]+atomic_masses[i]*coords[i,0]
+		com[1]=com[1]+atomic_masses[i]*coords[i,1]
+		com[2]=com[2]+atomic_masses[i]*coords[i,2]
+	com=com/total_mass
+
+	# shift all coords by com
+	for i in range(coords.shape[0]):
+		coords[i,:]=coords[i,:]-com[:]
+
+	# build the 3x3 inertia matix
+	inertia_mat=np.zeros((3,3))
+	for i in range(atomic_masses.shape[0]):
+		inertia_mat[0,0]=inertia_mat[0,0]+atomic_masses[i]*(coords[i,1]**2.0+coords[i,2]**2.0)
+		inertia_mat[1,0]=inertia_mat[1,0]-atomic_masses[i]*(coords[i,0]*coords[i,1])
+		inertia_mat[2,0]=inertia_mat[2,0]-atomic_masses[i]*(coords[i,0]*coords[i,2])
+		inertia_mat[1,1]=inertia_mat[1,1]+atomic_masses[i]*(coords[i,0]**2.0+coords[i,2]**2.0)
+		inertia_mat[2,1]=inertia_mat[2,1]-atomic_masses[i]*(coords[i,1]*coords[i,2])
+		inertia_mat[2,2]=inertia_mat[2,2]+atomic_masses[i]*(coords[i,0]**2.0+coords[i,1]**2.0)
+
+	inertia_mat[0,1]=inertia_mat[1,0]
+	inertia_mat[0,2]=inertia_mat[2,0]
+	inertia_mat[1,2]=inertia_mat[2,1]
+
+	# find eigenvalues and eigenvectors of inertia matrix. 
+	eigVal,eigVecs = np.linalg.eigh(inertia_mat)
+
+	rotation_vecs=np.zeros((natoms*3,3))
+
+	for i in range(atomic_masses.shape[0]):
+		principal_x=coords[i,0]*eigVecs[0,0]+coords[i,1]*eigVecs[1,0]+coords[i,2]*eigVecs[2,0]
+		principal_y=coords[i,0]*eigVecs[0,1]+coords[i,1]*eigVecs[1,1]+coords[i,2]*eigVecs[2,1]
+		principal_z=coords[i,0]*eigVecs[0,2]+coords[i,1]*eigVecs[1,2]+coords[i,2]*eigVecs[2,2]
+		
+		rotation_vecs[i*3,0]=principal_y*eigVecs[0,2]-principal_z*eigVecs[0,1]
+		rotation_vecs[i*3+1,0]=principal_y*eigVecs[1,2]-principal_z*eigVecs[1,1]
+		rotation_vecs[i*3+2,0]=principal_y*eigVecs[2,2]-principal_z*eigVecs[2,1]
+
+		rotation_vecs[i*3,1]=principal_z*eigVecs[0,0]-principal_x*eigVecs[0,2]
+		rotation_vecs[i*3+1,1]=principal_z*eigVecs[1,0]-principal_x*eigVecs[1,2]
+		rotation_vecs[i*3+2,1]=principal_z*eigVecs[2,0]-principal_x*eigVecs[2,2]
+
+		rotation_vecs[i*3,2]=principal_x*eigVecs[0,1]-principal_y*eigVecs[0,0]
+		rotation_vecs[i*3+1,2]=principal_x*eigVecs[1,1]-principal_y*eigVecs[1,0]
+		rotation_vecs[i*3+2,2]=principal_x*eigVecs[2,1]-principal_y*eigVecs[2,0]
+
+	# now mass weight vectors:
+	for i in range(atomic_masses.shape[0]):
+		rotation_vecs[i*3,:]=rotation_vecs[i*3,:]*np.sqrt(atomic_masses[i])
+		rotation_vecs[i*3+1,:]=rotation_vecs[i*3+1,:]*np.sqrt(atomic_masses[i])
+		rotation_vecs[i*3+2,:]=rotation_vecs[i*3+2,:]*np.sqrt(atomic_masses[i])
+
+	return rotation_vecs
+
+#build the projector for rotation and translation
+def build_rot_trans_proj(coords,atomic_masses):
+	natoms=atomic_masses.shape[0]
+	# get rotation vectors
+	rotation_vecs=moments_of_inertia(coords,atomic_masses)
+
+	# now build full 6 projectors
+	constraint_vec=np.zeros((natoms*3,6))
+
+	# fill constraint vector. First do translation:
+	trans_vec=np.zeros((natoms*3,3))
+	for i in range(atomic_masses.shape[0]):
+		trans_vec[i*3,0]=np.sqrt(atomic_masses[i])
+		trans_vec[i*3+1,1]=np.sqrt(atomic_masses[i])
+		trans_vec[i*3+2,2]=np.sqrt(atomic_masses[i])
+	# normalize vectors
+	trans_vec[:,0]=trans_vec[:,0]/np.linalg.norm(trans_vec[:,0])
+	trans_vec[:,1]=trans_vec[:,1]/np.linalg.norm(trans_vec[:,1]) 
+	trans_vec[:,2]=trans_vec[:,2]/np.linalg.norm(trans_vec[:,2]) 	
+
+	constraint_vec[:,0]=trans_vec[:,0]
+	constraint_vec[:,1]=trans_vec[:,1]
+	constraint_vec[:,2]=trans_vec[:,2]
+
+	# now do rotation. Normalize rotation_vecs
+	rotation_vecs[:,0]=rotation_vecs[:,0]/np.linalg.norm(rotation_vecs[:,0])
+	rotation_vecs[:,1]=rotation_vecs[:,1]/np.linalg.norm(rotation_vecs[:,1])
+	rotation_vecs[:,2]=rotation_vecs[:,2]/np.linalg.norm(rotation_vecs[:,2])  
+
+	constraint_vec[:,3]=rotation_vecs[:,0]
+	constraint_vec[:,4]=rotation_vecs[:,1]
+	constraint_vec[:,5]=rotation_vecs[:,2]
+
+	# now build the projector
+	temp_proj=np.dot(constraint_vec,np.transpose(constraint_vec)) # proj=I-const*const^T
+	projector=identity_mat(natoms*3)
+	projector=projector-temp_proj
+
+	return projector
+	
+
+def mass_weigh_hessian(hessian,atomic_masses):
+	hessian_mw=hessian
+	# loop over atoms i
+	for i in range(atomic_masses.shape[0]):
+
+		# loop over atoms j
+		for j in range(atomic_masses.shape[0]):
+			scale_fac=1.0/(np.sqrt(atomic_masses[i])*np.sqrt(atomic_masses[j]))
+			hessian_mw[3*i,3*j]=scale_fac*hessian_mw[3*i,3*j]
+			hessian_mw[3*i,3*j+1]=scale_fac*hessian_mw[3*i,3*j+1]
+			hessian_mw[3*i,3*j+2]=scale_fac*hessian_mw[3*i,3*j+2]
+			hessian_mw[3*i+1,3*j]=scale_fac*hessian_mw[3*i+1,3*j]
+			hessian_mw[3*i+1,3*j+1]=scale_fac*hessian_mw[3*i+1,3*j+1]
+			hessian_mw[3*i+1,3*j+2]=scale_fac*hessian_mw[3*i+1,3*j+2]
+			hessian_mw[3*i+2,3*j]=scale_fac*hessian_mw[3*i+2,3*j]
+			hessian_mw[3*i+2,3*j+1]=scale_fac*hessian_mw[3*i+2,3*j+1]
+			hessian_mw[3*i+2,3*j+2]=scale_fac*hessian_mw[3*i+2,3*j+2]
+
+	return hessian_mw
+
+# returns an identity matrix of dimension "dimension"
+def identity_mat(dimension):
+	identity=np.zeros((dimension,dimension))
+	for i in range(identity.shape[0]):
+		identity[i,i]=1.0
+
+	return identity
+
+
+# hessian has the format (ix,iy,iz,jx,jy,jz....)
+# frozen list contains a list of zeros and 1s, where 0s represent unfrozen atoms and 1s frozen atoms
+# if there are no frozen atoms, this is just a dummy list
+def freqs_NM_from_hessian(hessian, coords,atomic_masses,frozen_atoms,frozen_list): 
+	natoms=coords.shape[0]
+	constraints=6
+
+	# check if we have frozen atoms
+	if frozen_atoms !=0:
+		constraints=frozen_atoms*3  
+
+	# in a first step, build projector for frozen atoms
+	constraint_vec=np.zeros((natoms*3,constraints))  	
+
+	if frozen_atoms !=0:  # do not have to deal with projecting out rotation/tranlation
+		# Start with the case of actual frozen atoms
+		# loop over atoms
+		constraint_count=0
+		for i in range(natoms):
+			if frozen_list[i]==1:  # this is a frozen atom
+				for j in range(3): #loop over x,y,z	
+					temp_vec=np.zeros(natoms*3)
+					temp_vec[i*3+j]=np.sqrt(atomic_masses[i])
+					
+					constraint_vec[:,constraint_count]=temp_vec/(np.linalg.norm(temp_vec))
+					constraint_count=constraint_count+1
+
+		# build the effective projector
+		temp_proj=np.dot(constraint_vec,np.transpose(constraint_vec)) # proj=I-const*const^T
+		projector=identity_mat(natoms*3)
+		projector=projector-temp_proj
+
+	else: # no frozen atoms, but this means we have to build projectors for rotation/translation
+		projector=build_rot_trans_proj(coords,atomic_masses)		
+
+	hessian_mw=mass_weigh_hessian(hessian,atomic_masses)   # mass weigh hessian
+
+	hessian_mw_proj=np.dot(projector,np.dot(hessian_mw,projector))   #Proj*Hessian_MW*Proj
+
+	# now compute eigenvalues and eigenvectors of this hessian. 
+	eigVal,eigVecs = np.linalg.eigh(hessian_mw_proj)
+	freqs=np.zeros(eigVal.shape[0])
+	for i in range(freqs.shape[0]):
+		freqs[i]=np.sign(eigVal[i])*np.sqrt(np.absolute(eigVal[i])) 
+	
+	constrained_freqs=np.zeros(freqs.shape[0]-constraints)
+	constrained_nm=np.zeros((freqs.shape[0],freqs.shape[0]-constraints)) # this vector contains columns of normalized evecs
+									     # corresponding to the non-zero eigenvalues
+	constraint_counter=0
+	evec_counter=0
+	for x in freqs:
+		if abs(x)*const.hessian_freqs_to_cm>0.001:
+			constrained_freqs[constraint_counter]=x*const.hessian_freqs_to_cm/const.Ha_to_cm  # work out unit conversions here!
+			constrained_nm[:,constraint_counter]=eigVecs[:,evec_counter]
+			constraint_counter=constraint_counter+1
+		evec_counter=evec_counter+1
+
+	# return frequencies of constrained hessian in Ha, normalized unitless Normal mode vectors
+	return constrained_freqs,constrained_nm
