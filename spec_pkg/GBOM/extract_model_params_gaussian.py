Fixed a number of small issues with the vertical gradient approach

spec_pkg/GBOM/extract_model_params_gaussian.py
@@ -95,14 +95,17 @@ def extract_normal_mode_freqs_vertical_gradient(filename,num_modes):
                 linefile=open(filename,"r")
                 lines=linefile.readlines()
                 current_line=lines[freq_line].split()
-                if int(current_line[3])<num_modes: # no frozen atoms
-                        num_atoms_chromophore=(num_modes+6)/3
+                if int(current_line[3])<num_modes+1: # no frozen atoms
+                        num_atoms_chromophore=int((num_modes+6)/3)
                         frozen_atoms=0
                         total_num_atoms=num_atoms_chromophore
                 else:   # frozen atoms
-                        total_num_atoms=(int(current_line[3])+6)/3
+                        print('Frozen atoms?')
+                        total_num_atoms=int(((current_line[3])+6)/3)
                         frozen_atoms=total_num_atoms-num_atoms_chromophore
+                        print(total_num_atoms,frozen_atoms,num_atoms_chromophore)
         # Done
+        print(num_atoms_chromophore)
 
         # also return the normal modes
         nm_vec=np.zeros((num_modes,num_atoms_chromophore*3))

spec_pkg/GBOM/gbom_cumulant_response.py
@@ -809,7 +809,7 @@ def compute_corr_func_dipole_energy_cl_t(freqs_gs,dipole_mom,reorg_dipole,dipole
     return val
 
 # full HT function
-def full_HT_term(freqs_gs,Kmat,Jmat,Omega_sq,gamma,dipole_mom,dipole_deriv,kbT,max_t,num_points,decay_length,is_qm,is_3rd_order,stdout):
+def full_HT_term(freqs_gs,Kmat,Jmat,Omega_sq,gamma,dipole_mom,dipole_deriv,kbT,max_t,num_points,decay_length,is_qm,is_3rd_order,dipole_dipole_only,stdout):
     # Before doing anything, compute the dipole dipole correlation function
     Jtrans=np.transpose(Jmat)
     HT_func=np.zeros((num_points, 2), dtype=complex)
@@ -856,9 +856,9 @@ def full_HT_term(freqs_gs,Kmat,Jmat,Omega_sq,gamma,dipole_mom,dipole_deriv,kbT,m
     for i in range(num_points):
         HT_func[i,0]=t
         if is_qm:
-            HT_func[i,1]=HT_qm_t(freqs_gs,Kmat,Jtrans,Omega_sq,gamma,n_i_vec,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,t) 
+            HT_func[i,1]=HT_qm_t(freqs_gs,Kmat,Jtrans,Omega_sq,gamma,n_i_vec,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,dipole_dipole_only,t) 
         else:
-            HT_func[i,1]=HT_cl_t(freqs_gs,cross_corr_freq,Kmat,Jtrans,Omega_sq,gamma,n_i_vec,kbT,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,t)
+            HT_func[i,1]=HT_cl_t(freqs_gs,cross_corr_freq,Kmat,Jtrans,Omega_sq,gamma,n_i_vec,kbT,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,dipole_dipole_only,t)
         t=t+step_length
 
     return HT_func
@@ -884,7 +884,7 @@ def one_deriv_term_intergrant(cross_corr_func_freq,kbT,t):
 
 
 
-def HT_cl_t(freqs_gs,cross_corr_func_freq,Kmat,Jtrans,Omega_sq,gamma,n,kBT,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,t):
+def HT_cl_t(freqs_gs,cross_corr_func_freq,Kmat,Jtrans,Omega_sq,gamma,n,kBT,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,dipole_dipole_only,t):
     one_deriv_term=0.0+0.0*1j
     # 2nd order HT term is unchanged from qm version of the same term.
     for i in range(freqs_gs.shape[0]):
@@ -950,12 +950,15 @@ def HT_cl_t(freqs_gs,cross_corr_func_freq,Kmat,Jtrans,Omega_sq,gamma,n,kBT,dipol
 
         total=total_renorm_dipole+one_deriv_term+two_deriv_term+dUdUdmu+dmudUdU+dmudUdmu
     else:
-        total=total_renorm_dipole+one_deriv_term+two_deriv_term
+        if dipole_dipole_only:  # only two derivative term
+            total=total_renorm_dipole+two_deriv_term
+        else:
+            total=total_renorm_dipole+one_deriv_term+two_deriv_term
 
     return total
 
 # HT prefactor for a given value of t if the exact quantum correlation function is used. 
-def HT_qm_t(freqs_gs,Kmat,Jtrans,Omega_sq,gamma,n,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,t):
+def HT_qm_t(freqs_gs,Kmat,Jtrans,Omega_sq,gamma,n,dipole_mom,Jdip_deriv,reorg_dipole,total_renorm_dipole,is_3rd_order,dipole_dipole_only,t):
     one_deriv_term=0.0+0.0*1j
     for i in range(freqs_gs.shape[0]):
         term_xyz=-2.0*(np.dot(reorg_dipole-dipole_mom,Jdip_deriv[i,:]))*gamma[i]/(4.0*math.pi*(freqs_gs[i])**2.0)*((n[i]+1.0)*(1.0-cmath.exp(-1j*freqs_gs[i]*t))-n[i]*(1.0-cmath.exp(1j*freqs_gs[i])*t))
@@ -1017,7 +1020,10 @@ def HT_qm_t(freqs_gs,Kmat,Jtrans,Omega_sq,gamma,n,dipole_mom,Jdip_deriv,reorg_di
 
         total=total_renorm_dipole+one_deriv_term+dUdUdmu+dmudUdU+two_deriv_term+dmudUdmu
     else:
-        total=total_renorm_dipole+one_deriv_term+two_deriv_term
+        if dipole_dipole_only:
+            total=total_renorm_dipole+two_deriv_term
+        else:
+            total=total_renorm_dipole+one_deriv_term+two_deriv_term
 
     return total
 

spec_pkg/Morse/morse.py
@@ -818,6 +818,7 @@ class morse_coupled:
                         sys.exit('Error: Coupled Morse oscillator code currently only implemented for exactly 2 coupled oscillators.')
                 self.morse_oscs = []
                 for i in range(num_morse_oscillators):
+                        print(i,D_gs[i],D_ex[i])
                         self.morse_oscs.append(morse(D_gs[i],D_ex[i],alpha_gs[i],alpha_ex[i],mu[i],K[i],E_adiabatic,dipole_mom,max_states_gs,max_states_ex,num_points))
                 self.num_morse_oscillators=num_morse_oscillators
 
@@ -835,8 +836,11 @@ class morse_coupled:
                 self.E_adiabatic=E_adiabatic
 
 		# variables defining the grid:
-                self.grid_start=np.array([self.morse_oscs[0].grid_start,self.morse_oscs[1].grid_start])
-                self.grid_end=np.array([self.morse_oscs[0].grid_end,self.morse_oscs[1].grid_end])
+                grid_start=min(self.morse_oscs[0].grid_start,self.morse_oscs[1].grid_start)
+                grid_end=max(self.morse_oscs[0].grid_end,self.morse_oscs[1].grid_end)
+                
+                self.grid_start=np.array([grid_start,grid_start])
+                self.grid_end=np.array([grid_end,grid_end])
                 self.grid_n_points=np.array([num_points,num_points])
 
 		# variables defining number of bound states per morse oscillator
@@ -852,7 +856,7 @@ class morse_coupled:
                         self.n_max_gs[i]=self.morse_oscs[i].n_max_gs
                         self.n_max_ex[i]=self.morse_oscs[i].n_max_ex
                         eff_shift_vec_GBOM[i]=self.morse_oscs[i].K/self.morse_oscs[i].freq_gs # need to convert this to a dimensionless shift vec
-
+                print(self.gs_freqs,self.ex_freqs)
 
                 eff_0_0=E_adiabatic-0.5*np.sum(self.gs_freqs)+0.5*np.sum(self.ex_freqs)
                 self.eff_gbom=gbom.gbom(self.gs_freqs,self.ex_freqs,self.J,eff_shift_vec_GBOM,eff_0_0,dipole_mom,stdout)

spec_pkg/cumulant/extract_MD_params_terachem.py
@@ -215,9 +215,9 @@ def get_full_energy_dipole_moms_from_MD(filename_energies,filename_geoms,num_ato
     ref_geom=get_coors_frame(skip_snapshots,num_atoms,filename_geoms)
 
     # HACK:
-    masses[0]=masses[0]+4.0   # break symmetry for applying the Eckart conditions
-    masses[9]=masses[9]+6.0
-    masses[7]=masses[7]+4.0
+#    masses[0]=masses[0]+4.0   # break symmetry for applying the Eckart conditions
+#    masses[9]=masses[9]+6.0
+#    masses[7]=masses[7]+4.0
 
     prev_T=np.zeros((3,3))
     prev_T[0,0]=1.0
@@ -251,6 +251,7 @@ def get_full_energy_dipole_moms_from_MD(filename_energies,filename_geoms,num_ato
         prev_T=Tmat
 
         print('Iteration number  '+str(i))
+        print(energy_osc, dipole_transformed)
 
         # update energies dipole list
         energies_dipoles[i,0]=energy_osc[0]