Fixed a mistake in calculation of Franck-Condon emission spectra, where it breaks when the number of normal modes is odd.

spec_pkg/GBOM/franck_condon_response.py
@@ -461,10 +461,17 @@ def compute_full_response_func(
                 freq_gs, freq_ex, Jmat, Kmat, kBT, current_t
             )
 	
+
+        if is_emission and ((freq_gs.shape[0])%2)!=0:  # Check if this is an odd number of normal modes:
+            g_inf=g_inf+1j*math.pi
+            lineshape[counter, 1] = g_inf.real
+            chi[counter, 1] = -chi_t[0]   # For Emission, there is an additional factor of (-i)^N, which for odd normal modes, gives a factor of -i*Ch(t)
+            chi[counter, 2] = chi_t[1]+math.pi/2.0   
+        else:
+            chi[counter, 1] = chi_t[0]   
+            chi[counter, 2] = chi_t[1]
+            lineshape[counter,1]=g_inf.real
         stdout.write("%5d      %10.4f          %10.4e       %10.4e" % (counter+1,current_t*const.fs_to_Ha, np.real(g_inf), np.imag(g_inf))+'\n')
-        lineshape[counter, 1] = g_inf.real
-        chi[counter, 1] = chi_t[0]
-        chi[counter, 2] = chi_t[1]
         counter = counter + 1
 
     np.savetxt("FC_lineshape_function.dat", lineshape)